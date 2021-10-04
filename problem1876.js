0f71a4a1ab3969c6c0a9049c1372383691dad2d7
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-10-04 17:09:30 -0400

    adds problem 1876 solution

---
+++ problem1876.js
@@ -0,0 +1,19 @@
+// original submission
+// 80 ms (faster than 74.85%); 40.5 MB (less than 56.44%)
+const countGoodSubstrings = s => {
+  const length = 3
+  let numberOfGoods = 0
+  
+  const substrings = []
+  for(let i=0; i<=s.length - length; i++){
+      const substring = s.slice(i,i+length)
+      substrings.push(substring)
+  }
+  substrings.forEach(sub => {
+      const uniqueLetters = [...new Set(sub.split(''))]
+      if(uniqueLetters.length === length){
+          numberOfGoods++
+      }
+  })
+  return numberOfGoods
+};
