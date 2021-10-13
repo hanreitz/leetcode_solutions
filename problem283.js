b55dde4b32dc77f49347a3ae062d7e5c818623e7
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-10-12 22:16:36 -0400

    adds two-pointer solution to move zeroes problem 283

---
+++ problem283.js
@@ -0,0 +1,15 @@
+// two-pointer problem
+var moveZeroes = function(nums) {
+  let j = 0;
+  
+  for(let i = 0; i < nums.length; i++){
+      if(nums[i] !== 0){
+          nums[j] = nums[i]
+          j++
+      }
+  }
+  
+  for(i = j; i < nums.length; i++){
+      nums[i] = 0
+  }
+};
