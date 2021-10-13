657a6c421e839af615e5cd1d658f8242652c333e
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-10-13 09:59:11 -0400

    adds two-pointer solution to problem 344

--- problem344.js
+++ problem344.js
@@ -9,3 +9,14 @@ var reverseString = function(s) {
       s[i] = right
   }
 };
+
+// a much better solution with two-pointer approach!
+
+var reverseString = function(s) {
+  let start = 0;
+  let end = s.length - 1;
+  
+  while(start < end){
+      [s[start++], s[end--]] = [s[end], s[start]]
+  }
+}
