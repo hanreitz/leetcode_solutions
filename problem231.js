70fa81cc361dc27379842b90fee9f41f006462f5
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-10-31 17:15:24 -0400

    adds bit manipulation to problem 231

--- problem231.js
+++ problem231.js
@@ -30,3 +30,10 @@ const isPowerOfTwo = n => {
   if(n <= 0 || n % 2 !== 0) return false
   return isPowerOfTwo(n/2)
 }
+
+// bit manipulation solution
+const isPowerOfTwo = n => {
+  if(n < 1) return false
+  
+  return !(n & (n - 1))
+}
